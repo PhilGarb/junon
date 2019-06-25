8b28b5e3d305e8802f534d6b1bc72eb9c9e6add7
Philipp Garbowsky <phil.garbowsky@philtech.dev>
2019-06-25 16:23:17 +0200

    small link fix

--- src/components/UI Components/Link.js
+++ src/components/UI Components/Link.js
@@ -7,14 +7,14 @@ const StyledLink = styled(Link)`
   padding: 0.5em;
   margin: 0.5em;
   min-width: max-content;
-  border-bottom: 10px solid transparent;
+  border-bottom: 0.3em solid transparent;
   border-radius: 10px;
   transition: all 200ms ease;
   display: inline-block;
 
   :hover,
   :focus {
-    border-bottom: 10px solid rgba(var(--tertiary-color), 0.7);
+    border-bottom: 0.3em solid rgba(var(--tertiary-color), 0.7);
     background: rgba(var(--tertiary-color), 0.2);
   }
 `
